1826a05a0ed03567bcddb4383e4cd6d83715741c
cyberbobjr@yahoo.com <cyberbobjr@yahoo.com>
2018-12-13 23:31:18 +0100

    display stats

--- src/app/modules/game/pages/info-page/info-page.component.ts
+++ src/app/modules/game/pages/info-page/info-page.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EventLog} from '../../../../core/classes/event-log';
+import {AttributesFactory} from '../../../../core/factories/attributes-factory';
+import {EntitiesService} from '../../services/entities.service';
 
 @Component({
              selector: 'app-info-page',
@@ -8,11 +10,16 @@ import {EventLog} from '../../../../core/classes/event-log';
            })
 export class InfoPageComponent implements OnInit {
   message: string = null;
+  attributes: Array<string> = [];
 
-  constructor() {
+  constructor(private _entitiesService: EntitiesService) {
   }
 
   ngOnInit() {
+    for (const [attribute, value] of AttributesFactory.getAttributes()) {
+      this.attributes.push(attribute);
+    }
+
     EventLog.getInstance()
             .message$
             .subscribe((message: string) => {
