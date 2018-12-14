40acf0fad569346c9420c24c1ce2ab58670108e3
Benjamin MARCHAND <cyberbobjr@gmail.com>
2018-12-14 19:10:09 +0100

    display player info

--- src/app/modules/game/pages/info-page/info-page.component.ts
+++ src/app/modules/game/pages/info-page/info-page.component.ts
@@ -2,29 +2,32 @@ import {Component, OnInit} from '@angular/core';
 import {EventLog} from '../../../../core/classes/event-log';
 import {AttributesFactory} from '../../../../core/factories/attributes-factory';
 import {EntitiesService} from '../../services/entities.service';
+import {Player} from '../../../../core/classes/entities/player';
 
 @Component({
-             selector: 'app-info-page',
-             templateUrl: './info-page.component.html',
-             styleUrls: ['./info-page.component.css']
-           })
+  selector: 'app-info-page',
+  templateUrl: './info-page.component.html',
+  styleUrls: ['./info-page.component.css']
+})
 export class InfoPageComponent implements OnInit {
   message: string = null;
   attributes: Array<string> = [];
+  private _player: Player = null;
 
   constructor(private _entitiesService: EntitiesService) {
   }
 
   ngOnInit() {
+    this._player = this._entitiesService.player as Player;
     for (const [attribute, value] of AttributesFactory.getAttributes()) {
       this.attributes.push(attribute);
     }
 
     EventLog.getInstance()
-            .message$
-            .subscribe((message: string) => {
-              this.message = message;
-            });
+      .message$
+      .subscribe((message: string) => {
+        this.message = message;
+      });
   }
 
 }
