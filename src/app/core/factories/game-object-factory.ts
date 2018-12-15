8125020c463a980a696d099140879b5b54074f5c
cyberbobjr@yahoo.com <cyberbobjr@yahoo.com>
2018-12-15 16:05:47 +0100

    Register weapons in Factory

--- src/app/core/factories/game-object-factory.ts
+++ src/app/core/factories/game-object-factory.ts
@@ -1,13 +1,16 @@
-import * as weapon from '../rules/object/weapons.json';
+import * as weapons from '../rules/object/weapons.json';
 import {Weapon} from '../classes/base/weapon';
+import {Utility} from '../classes/utility';
 
 export class GameObjectFactory {
   private static instance: GameObjectFactory;
+  private _weapons: Array<Weapon> = [];
 
   constructor() {
-    console.log(weapon[0]);
-    const weaponObject: Weapon = new Weapon(weapon[0]);
-    console.log(weaponObject);
+    console.log('GameObjectFactory created');
+    for (const key of Object.keys(weapons.default)) {
+      this._weapons.push(new Weapon(weapons.default[key]));
+    }
   }
 
   static getInstance() {
@@ -17,7 +20,14 @@ export class GameObjectFactory {
     return GameObjectFactory.instance;
   }
 
-  generateRandomWeapon() {
+  getRandomWeapon(): Weapon {
+    const randomNumber = Utility.rolldice(this._weapons.length);
+    return this._weapons[randomNumber];
+  }
 
+  getWeaponByName(weaponName: string): Weapon | null {
+    return this._weapons.find((weapon: Weapon) => {
+      return weapon.name === weaponName;
+    });
   }
 }
