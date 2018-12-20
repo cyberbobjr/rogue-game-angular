295a2998fea5d790f9f302c15dcd82ce4a4737d8
cyberbobjr@yahoo.com <cyberbobjr@yahoo.com>
2018-12-20 18:02:13 +0100

    display inventory ui

--- src/app/modules/game/pages/inventory-page/inventory-page.component.ts
+++ src/app/modules/game/pages/inventory-page/inventory-page.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EntitiesService} from '../../services/entities.service';
 
 @Component({
-  selector: 'app-inventory-page',
-  templateUrl: './inventory-page.component.html',
-  styleUrls: ['./inventory-page.component.css']
-})
+             selector: 'app-inventory-page',
+             templateUrl: './inventory-page.component.html',
+             styleUrls: ['./inventory-page.component.css']
+           })
 export class InventoryPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _entitiesService: EntitiesService) {
+  }
 
   ngOnInit() {
   }
