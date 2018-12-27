06279fa0fb7c9a5f1d9076e6bfd761b5ffd22854
cyberbobjr@yahoo.com <cyberbobjr@yahoo.com>
2018-12-27 18:48:34 +0100

    Random mouvement for monster when idle

--- src/app/core/classes/actions/idle-action.ts
+++ src/app/core/classes/actions/idle-action.ts
@@ -4,6 +4,11 @@ import {ActionResult} from './action-result';
 import {EventLog} from '../event-log';
 import {ChaseAction} from './chase-action';
 import {GameEngineService} from '../../../modules/game/services/game-engine.service';
+import {Position} from '../base/position';
+import {Direction} from '../../enums/direction.enum';
+import {MapEngine} from '../../../modules/game/services/map-engine.service';
+import {Tile} from '../base/tile';
+import {Utility} from '../utility';
 
 export class IdleAction implements Iaction {
   private _info = '';
@@ -15,11 +20,24 @@ export class IdleAction implements Iaction {
     if (actor.sprite.light) {
       EventLog.getInstance().message = 'Player in sight !';
       actor.setNextAction(new ChaseAction(actor));
+    } else {
+      const direction: Direction = this._getRandomDirection();
+      const destPosition: Position = actor.position.computeDestination(direction);
+      const mapEngine: MapEngine = gameEngine.mapEngine;
+      const tile: Tile = <Tile>mapEngine.getTileOrEntityAt(destPosition);
+      if (tile instanceof Tile && tile.isWalkable()) {
+        actor.position = destPosition;
+        tile.onWalk(actor);
+      }
     }
     return ActionResult.SUCCESS;
   }
 
   getInfo(): string {
     return this._info;
+  }
+
+  private _getRandomDirection(): Direction {
+    return Direction.getRandom();
   }
 }
