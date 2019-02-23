14342a43139f121f1f819e72988f3a5330420922
Benjamin MARCHAND <cyberbobjr@gmail.com>
2019-02-23 17:02:33 +0100

    fix display inventory page info

--- src/app/modules/game/pages/inventory-page/inventory-page.component.ts
+++ src/app/modules/game/pages/inventory-page/inventory-page.component.ts
@@ -5,18 +5,19 @@ import {SlotType} from '../../../../core/enums/equiped-type.enum';
 import {Utility} from '../../../../core/classes/utility';
 
 @Component({
-             selector: 'app-inventory-page',
-             templateUrl: './inventory-page.component.html',
-             styleUrls: ['./inventory-page.component.css']
-           })
+  selector: 'app-inventory-page',
+  templateUrl: './inventory-page.component.html',
+  styleUrls: ['./inventory-page.component.css']
+})
 export class InventoryPageComponent implements OnInit {
-  player: Player = null;
-
   constructor(private _entitiesService: EntitiesManager) {
   }
 
   ngOnInit() {
-    this.player = this._entitiesService.getPlayer();
+  }
+
+  getPlayer(): Player {
+    return this._entitiesService.getPlayer();
   }
 
   getSlotTypeLabel(slot: SlotType) {
